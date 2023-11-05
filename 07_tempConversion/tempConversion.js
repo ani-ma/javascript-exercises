40cd225e09b2aa5a7fad405df44cd50051536ece
Thordark <78201550+someguy2104@users.noreply.github.com>
2023-11-05 11:15:44 +0530

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  c = (f-32)*(5/9);
+  
+  c = Math.round(c,2);
+  return c;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(ce) {
+  fa = ce*(9/5)+32;
+  fa = Math.round(fa,2);
+  return fa;
 };
+
+a = convertToCelsius(32) // fahrenheit to celsius, should return 0
+console.log(a);
+b = convertToFahrenheit(0)
+console.log(b)
+
+convertToCelsius(32) // fahrenheit to celsius, should return 0
+
+convertToFahrenheit(0)
 
 // Do not edit below this line
 module.exports = {
